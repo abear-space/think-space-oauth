c019713b04903a8acf437d9f30439bb379410d32
abearxiong pc <xiongxiao1012@outlook.com>
2020-11-08 11:39:29 +0800

    fix: index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,16 @@
 export interface GithubTokenOptions {
-  authorizeUrl: string;
-  accessTokenUrl: string;
-  client_id: string;
-  client_secret: string;
-  proxyUrl: string;
+  authorizeUrl?: string;
+  accessTokenUrl?: string;
+  client_id?: string;
+  client_secret?: string;
+  proxyUrl?: string;
 }
-
-export interface GitHubToken {
-  login: any;
-  isNext: any;
-  next: any;
-  getToken: any;
-  logout: any;
-  auto: Promise<any>;
+export interface GithubTokenValue {
+  login: () => any;
+  isNext: () => any;
+  next: () => any;
+  getToken: () => any;
+  logout: () => any;
+  auto: () => Promise<any>;
 }
+declare const GithubToken:  (githubToenOptions: GithubTokenOptions) => GithubTokenValue;
